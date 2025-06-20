e5599eabc2093705eb1d38b4101484bcecc94187
lucsvp <lucasvieirapalavro@gmail.com>
2025-06-20 10:47:36 -0300

    Code WebApp Sketch

--- log_in_system/urls.py
+++ log_in_system/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name="index"),
+    path('signin/', views.signin, name="signin"),
+    path('login/', views.login, name="login"),
 ]
